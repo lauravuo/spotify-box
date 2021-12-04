bf58a9e53bf111d68048deeb97de0b6233ffcb24
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2021-12-04 22:53:19 +0200

    Add markdown version of output.

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const { getTopTracks } = require('./spotify')
 async function main() {
   const tracksJson = await getTopTracks()
   fs.writeFileSync('./tracks.json', JSON.stringify(tracksJson))
+  const data = tracksJson.items.map(item => ({
+    artist: item.artists[0].name,
+    name: item.name,
+    url: item.external_urls.spotify,
+    image: item.album.images[0].url
+  } ))
+  const md = "# Top Tracks"
+  const mdWithData = data.reduce(
+    (result, item) =>
+      result+ `\n\n## [${item.artist}: ${item.name}](${item.url})\n![${item.artist}!](${item.image})`, md)
+  fs.writeFileSync("./tracks.md", mdWithData)
 }
 
 ;(async () => {
